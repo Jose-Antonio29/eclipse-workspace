5b9192bc6e17faf29fcca8eb4ae2c7a7b639990c
José Antonio Dias <joseantonio.dias.rodrigues@ces-vegamedia.es>
2026-02-24 09:25:54 +0100

    24/02/26

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/46/90d92f8b541100111889fc7abba5e5f3
@@ -0,0 +1,16 @@
+package colecciones.listas;
+
+import java.util.ArrayList;
+
+public class ArrayList02 {
+
+	public static void main(String[] args) {
+		// Creamos una lista de números enteros
+		ArrayList<Integer> numberList = new ArrayList<Integer>();
+		
+		// Añadimos 6 valores a la lista
+		
+
+	}
+
+}
--- Unidad_Trabajo_5/src/colecciones/listas/ArrayList02.java
+++ Unidad_Trabajo_5/src/colecciones/listas/ArrayList02.java
@@ -6,9 +6,29 @@ public class ArrayList02 {
 
 	public static void main(String[] args) {
 		// Creamos una lista de números enteros
-		ArrayList<Integer> numberList = new ArrayList<Integer>();
+		ArrayList<Integer> lista = new ArrayList<Integer>();
 		
 		// Añadimos 6 valores a la lista
+		lista.add(1);
+		lista.add(5);
+		lista.add(7);
+		lista.add(1);
+		lista.add(14);
+		lista.add(21);
+		lista.add(13);
+		
+		// Mostramos la lista de valores
+		System.out.println("Lista: "+lista);
+		
+		// Eliminar el valor que ocupa la posición 5 en la lista
+		System.out.println("\nEliminamos el valor que ocupa la posición 5 en la lista");
+		lista.remove(5);
+		System.out.println("Lista: "+lista);
+
+		// Eliminamos el valor 5 de la lista
+		System.out.println("\nEliminamos el valor 5 de la lista");
+		lista.remove(Integer.valueOf(5));
+		System.out.println("Lista: "+lista);
 		
 
 	}
